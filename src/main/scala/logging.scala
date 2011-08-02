7a3bedd6ede4d54f8a674744d5b9245eebf35b63
Aljoscha Leonhardt <apleonhardt@gmail.com>
2011-08-02 14:24:02 +0200

    Implement logging helper trait (using Twitter util-logging)

---
+++ src/main/scala/logging.scala
@@ -0,0 +1,28 @@
+package org.gnode.log
+
+import com.twitter.logging.{Logger, Level, Policy}
+import com.twitter.logging.config._
+
+// CONVENIENCE TRAIT FOR LOGGING VIA TWITTER/UTIL-LOGGER
+
+trait Loggable {
+
+  var logger: Logger = null
+
+  // Called from extended class before first log; provides logger: Logger
+  def logInit(logNode: String = "", logFile: String = "/home/aleonhardt/log/gnode/dev.log"): Unit = {
+
+    val logConfig = new LoggerConfig {
+      node = logNode
+      level = Level.DEBUG
+      handlers = new FileHandlerConfig {
+	filename = logFile
+	roll = Policy.Never
+      }
+    }
+
+    logger = logConfig()
+
+  } 
+
+}
