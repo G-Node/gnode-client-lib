21dee776d583ee25782f96c6d49b00cc59d1f756
Aljoscha Leonhardt <apleonhardt@gmail.com>
2011-09-14 22:45:00 +0200

    Add scaffolding for parser, session, NEObject

---
+++ src/main/scala/parser.scala
@@ -0,0 +1,15 @@
+package org.gnode.lib.parse
+
+import org.gnode.lib.neo._
+import org.gnode.lib.util._
+import org.gnode.lib.api._
+
+import net.liftweb.json._
+import dispatch._
+
+object NEOParser {
+
+  def make(data: String): Option[NEObject] = {}
+  def apply(data: String) = make data
+
+}
---
+++ src/main/scala/session.scala
@@ -0,0 +1,8 @@
+package org.gnode.lib
+
+import org.gnode.lib.conf._
+import org.gnode.lib.util._
+
+class Session {
+
+  
