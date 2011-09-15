6f11cf15176c4c6760889e1efba0387083983514
Aljoscha Leonhardt <apleonhardt@gmail.com>
2011-09-15 12:03:44 +0200

    Refactor Reader

--- src/main/scala/parser.scala
+++ src/main/scala/parser.scala
@@ -5,59 +5,90 @@ import org.gnode.lib.util._
 import org.gnode.lib.api._
 
 import net.liftweb.json._
-import net.liftweb.json.JsonParser.ParseException
 import dispatch._
 
-object NEOParser extends Loggable {
-  
-  def apply(data: String) = make(data)
+class ExtractError extends Exception("NEO object could not be parsed")
 
-  def make(data: String): NEObject =
-    makeOpt(data).getOrElse(null)
+object Reader extends Loggable {
   
-  def makeOpt(data: String): Option[NEObject] = {
+  def apply(data: String) = makeObject(data)
+
+  def makeObject(data: String): NEObject =
+    makeObjectOpt(data) match {
+      case Some(n) => n
+      case None => throw new ExtractError
+    }
+  
+  def makeObjectOpt(data: String): Option[NEObject] = {
     
     import scala.collection.mutable.{ListBuffer, Map => MuMap}
+    import net.liftweb.json.JsonParser.ParseException
 
-    val parsedData = parse(data)
+    var p: Option[JValue] = None
+    
+    try {
+      p = Some(parse(data))
+    } catch {
+      case e: ParseException =>
+	logger.error("Parse exception occured")
+	return None
+      case _ =>
+	logger.error("Unknown error occured")
+	return None
+    }
+
+    val parsedData = p get
 
     val strMap = MuMap[String, String]()
     val numMap = MuMap[String, Double]()
     val dataMap = MuMap[String, NEOData]()
 
-    def notData(l: List[JField]): Boolean =
-      !(l exists { f: JField => f.name == "units" })
+    def isData(l: List[JField]) =
+      l exists { f: JField => f.name == "units" }
 
-    def isData(l: List[JField]) = !notData(l)
-
+    def notData(l: List[JField]) =
+      !isData(l)
+     
     // Extract string-based info
-    for {JObject(list) <- parsedData
-	 if notData(list)
-	 JField(key, JString(value)) <- list} { strMap += key -> value }
+    for {
+      JObject(list) <- parsedData
+      if notData(list)
+      JField(key, JString(value)) <- list
+    } {
+      strMap += key -> value
+    }
 
     // Extract double- and integer-based info
-    for {JObject(list) <- parsedData
-	 if notData(list)
-	 JField(key, JInt(value)) <- list} { numMap += key -> value.toDouble }
+    for {
+      JObject(list) <- parsedData
+      if notData(list)
+      JField(key, JInt(value)) <- list
+    } numMap += key -> value.toDouble
 
-    for {JObject(list) <- parsedData
-	 if notData(list)
-	 JField(key, JDouble(value)) <- list} { numMap += key -> value }
+    for {
+      JObject(list) <- parsedData
+      if notData(list)
+      JField(key, JDouble(value)) <- list
+    } numMap += key -> value
 
     // Extract data
-    for {JField(key, JObject(List(JField("units", JString(units)), JField("data", JDouble(data))))) <- parsedData}
-      { dataMap += key -> new NEOData(units, data) }
+    for {
+      JField(key, JObject(List(JField("units", JString(units)), JField("data", JDouble(data))))) <- parsedData
+    } dataMap += key -> new NEOData(units, data)
 
-    for {JField(key, JObject(List(JField("units", JString(units)), JField("data", JInt(data))))) <- parsedData}
-      { dataMap += key -> new NEOData(units, data.toDouble) }
+    for {
+      JField(key, JObject(List(JField("units", JString(units)), JField("data", JInt(data))))) <- parsedData
+    } dataMap += key -> new NEOData(units, data.toDouble)
 
-    for {JField(key, JObject(List(JField("units", JString(units)), JField("data", JArray(data))))) <- parsedData}
-      {
-	val buffer = ListBuffer[Double]()
-	for (JDouble(d) <- data) buffer += d
-
-	dataMap += key -> new NEOData(units, buffer.toList)
-      }
+    for {
+      JField(key, JObject(List(JField("units", JString(units)), JField("data", JArray(data))))) <- parsedData
+    } {
+      
+      val buffer = ListBuffer[Double]()
+      for (JDouble(d) <- data) buffer += d
+      dataMap += key -> new NEOData(units, buffer.toList)
+    
+    }
     
     // Build return object
     Some(new NEObject(Map.empty ++ strMap, Map.empty ++ numMap, Map.empty ++ dataMap))
