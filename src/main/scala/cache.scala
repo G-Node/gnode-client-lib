1b71ea9fef13d50f5de6c7e42ce040a83586fe31
Aljoscha Leonhardt <apleonhardt@gmail.com>
2011-09-15 23:05:17 +0200

    Finish memory cache proposal

--- src/main/scala/cache.scala
+++ src/main/scala/cache.scala
@@ -77,8 +77,41 @@ class MemoryCache extends Cache with Loggable {
   case class CacheObject(obj: NEObject, etag: String)
   
   import scala.collection.mutable.Map
-  private val cache = new Map[String, CacheObject]
+  private val cache = new Map[String, CacheObject]()
   
-  //def add(obj: NEObject, etag: String): Option[String]
+  def add(obj: NEObject, etag: String = "") {
 
+    val cObj = CacheObject(obj, etag)
+    val neo_id = obj.stringInfo.get("neo_id")
+
+    neo_id match {
+      case Some(id) =>
+	cache += (id -> cObj)
+      case _ =>
+	throw new IllegalArgumentException
+    }
+
+  }
+
+  def retrieve(id: String): Option[NEObject] =
+    cache.get(id) match {
+      case Some(CacheObject(obj, _)) => Some(obj)
+      case None => None
+    }
+
+  def objectTag(id: String): Option[String] =
+    cache.get(id) match {
+      case Some(CacheObject(_, etag)) => Some(etag)
+      case None => None
+    }
+
+  def delete(id: String) {
+    cache -= id
+  }
+
+  def replace(id: String, obj: NEObject, etag: String) {
+    delete(id)
+    add(obj, etag)
+  }
+  
 }
