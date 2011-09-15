524de7a2e449ebe0203aa0eb3db3b2a6e9fbf0ea
Aljoscha Leonhardt <apleonhardt@gmail.com>
2011-09-15 12:03:56 +0200

    Fix minor problems

--- src/main/scala/cache.scala
+++ src/main/scala/cache.scala
@@ -1,4 +1,16 @@
-package org.gnode.lib.cache
+// package org.gnode.lib.cache
 
-class Cache
-object CacheDispatcher
+// sealed abstract class CacheType
+
+// object CacheType {
+//   case object MONGO extends CacheType
+//   case object SQLITE extends CacheType
+//   case object MEMORY extends CacheType
+// }
+
+// object Cache {
+//   def apply(c: CacheType): Cache =
+
+// abstract class Cache
+
+// class MongoCache extends
---
+++ src/main/scala/client.scala
@@ -0,0 +1,17 @@
+// package org.gnode.lib.http
+
+// import dispatch._
+// import org.gnode.lib.cache._
+// import org.gnode.util._
+
+// trait HttpInteractor extends Logger {
+//   // Make Http object (with Logger)
+
+//   val http = new Http with Logger
+
+// }
+
+// class Downloader extends HttpInteractor {
+
+//   private val cache = new Cache
+// class Uploader extends HttpInteractor
