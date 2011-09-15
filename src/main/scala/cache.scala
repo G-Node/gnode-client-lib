7dfb977be4735d8a17e33a71b36ef09f3014f725
Aljoscha Leonhardt <apleonhardt@gmail.com>
2011-09-15 23:04:46 +0200

    Adjust base class for Cache

--- src/main/scala/cache.scala
+++ src/main/scala/cache.scala
@@ -41,12 +41,34 @@ object Cache {
 
 abstract class Cache {
 
-  // Caching
+  /* Caching
 
-  def add(obj: NEObject, etag: String): Option[String]
+  Suggested flow:
+  * Retrieving system checks if cache holds object with given ID
+    -> If not, retrieve/fail.
+  * If yes, retrieve E-Tag.
+  * Request with E-Tag. Check status code:
+    -> 200 + E-Tag
+	-> Store object in cache
+	-> Return
+    -> 304
+	-> Retrieve object from cache
+	-> Return
+  * If HTTP failure: Either return object from cache, or nothing!
+
+  */
+  
+  
+  // Constructive
+  def add(obj: NEObject, etag: String)
   def retrieve(id: String): Option[NEObject]
+
+  // Destructive
   def delete(id: String)
-  def replace(id: String, obj: NEObject, etag: String): Option[String]
+  def replace(id: String, obj: NEObject, etag: String)
+
+  // Information
+  def objectTag(id: String): Option[String]
 
 }
 
