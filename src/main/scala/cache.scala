2021833d7222cfd2808d7abb4d2bfedd69f60427
Aljoscha Leonhardt <apleonhardt@gmail.com>
2011-09-15 22:13:46 +0200

    Implement CacheType matcher

--- src/main/scala/cache.scala
+++ src/main/scala/cache.scala
@@ -1,16 +1,21 @@
-// package org.gnode.lib.cache
+package org.gnode.lib.cache
 
-// sealed abstract class CacheType
+import org.gnode.lib.neo.{NEOData, NEObject}
 
-// object CacheType {
-//   case object MONGO extends CacheType
-//   case object SQLITE extends CacheType
-//   case object MEMORY extends CacheType
-// }
+sealed abstract class CacheType
 
-// object Cache {
-//   def apply(c: CacheType): Cache =
+object CacheType {
+  
+  case object MONGO extends CacheType
+  case object SQLITE extends CacheType
+  case object MEMORY extends CacheType
 
-// abstract class Cache
+  def apply(cacheType: String): CacheType = cacheType match {
+ 
+    case "MONGO" => MONGO
+    case "SQLITE" => SQLITE
+    case "MEMORY" => MEMORY
+    case _ =>
+      throw new IllegalArgumentException
 
 // class MongoCache extends
