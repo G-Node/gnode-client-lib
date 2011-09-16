5360f1885740232c517363b4154881cc3521221d
Aljoscha Leonhardt <apleonhardt@gmail.com>
2011-09-16 10:48:19 +0200

    Implement basic HTTP interaction

--- src/main/scala/client.scala
+++ src/main/scala/client.scala
@@ -1,17 +1,24 @@
-// package org.gnode.lib.http
+package org.gnode.lib.client
 
-// import dispatch._
-// import org.gnode.lib.cache._
-// import org.gnode.util._
+import org.gnode.lib.cache._
+import org.gnode.lib.util._
+import org.gnode.lib.conf._
 
-// trait HttpInteractor extends Logger {
-//   // Make Http object (with Logger)
+import dispatch._
 
-//   val http = new Http with Logger
+trait HttpInteractor extends Loggable {
+  
+  // Create custom Http object that uses library-wide Twttr logging
+  lazy val http = new Http with Loggable {
+    override def make_logger = new Logger {
 
-// }
+      def info(msg: String, items: Any*) { logger.info(msg, items: _*) }
+      def warn(msg: String, items: Any*) { logger.warning(msg, items: _*) }
+      
+    }
+  }
 
-// class Downloader extends HttpInteractor {
+}
 
 //   private val cache = new Cache
 // class Uploader extends HttpInteractor
