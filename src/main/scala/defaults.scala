86cbe1c1caf336ee49333d78b00142e97a35ed00
Aljoscha Leonhardt <apleonhardt@gmail.com>
2011-09-19 20:59:25 +0200

    Add default TransferManager

--- src/main/scala/defaults.scala
+++ src/main/scala/defaults.scala
@@ -1,9 +1,13 @@
 package org.gnode.lib.conf
 
+import org.gnode.lib.client._
+
 object Default {
 
   val CONFIGURATION = ConfigurationReader.create("ray",
 						 "pass",
 						 "hal10.g-node.pri")
+
+  val TRANSFER_MANAGER = new TransferManager(CONFIGURATION)
 
 }
