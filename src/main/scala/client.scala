080f161215fd3f1efdb60ffb3305a722064dbf08
Aljoscha Leonhardt <apleonhardt@gmail.com>
2011-09-19 21:02:15 +0200

    Suppress Dispatch logging

--- src/main/scala/client.scala
+++ src/main/scala/client.scala
@@ -17,7 +17,7 @@ trait HttpInteractor extends Loggable {
   lazy val http = new Http with Loggable {
     override def make_logger = new Logger {
 
-      def info(msg: String, items: Any*) { logger.info(msg, items: _*) }
+      def info(msg: String, items: Any*) { /* logger.info(msg, items: _*) */ }
       def warn(msg: String, items: Any*) { logger.warning(msg, items: _*) }
       
     }
