e22fdf5313a94bb4e0e8a95aa8d65ce580ca9d2e
Aljoscha Leonhardt <apleonhardt@gmail.com>
2011-09-20 15:03:29 +0200

    Implement basic validator

---
+++ src/main/scala/validator.scala
@@ -0,0 +1,69 @@
+package org.gnode.lib.validation
+
+import org.gnode.lib.neo._
+import org.gnode.lib.conf._
+import org.gnode.lib.parse._
+import org.gnode.lib.util._
+import org.gnode.lib.util.File._
+
+import net.liftweb.json._
+import scala.collection.mutable.Map
+
+import org.gnode.lib.conf.LogMessages._
+
+case class Contract(required: List[String],
+		    attributes: List[String],
+		    data_fields: List[String],
+		    children: List[String],
+		    parents: List[String])
+
+class Validator(private val config: Configuration) extends Loggable {
+		      
+  private val contract: Map[String, Contract] =
+    readContractFromFile(config.apiDefinition).getOrElse(Map[String, Contract]())
+
+  // CONTRACT EXTRACTION METHODS
+  private def readContractFromFile(path: String): Option[Map[String, Contract]] =
+    try {
+      using(io.Source.fromFile(path)) { file =>
+	readContract(file.mkString) }
+    } catch {
+      case e: java.io.FileNotFoundException =>
+	logger error FILE_NOT_FOUND(path)
+	None
+    }
+
+  private def readContract(raw: String): Option[Map[String, Contract]] = {
+    
+    import net.liftweb.json.JsonParser.ParseException
+    implicit val formats = DefaultFormats
+
+    var p: Option[JValue] = None
+
+    try {
+      p = Some(parse(raw))
+    } catch {
+      case e: ParseException =>
+	logger error READ_ERROR_PARSE
+	return None
+      case _ =>
+	logger error READ_ERROR_UNKNOWN
+	return None
+    }
+
+    val reqs = p get
+
+    val rmap = for {
+      JField(t, defs @ JObject(_)) <- reqs
+    } yield t -> defs.extract[Contract]
+
+    return Some(Map(rmap: _*))
+
+  }
+
+  // VALIDATION METHODS
+
+  def validate(obj: NEObject): Boolean =
+    true
+
+}
