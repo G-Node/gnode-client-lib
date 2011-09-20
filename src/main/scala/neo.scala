897475f1a73731c66b3d081f320ddec5f66cc036
Aljoscha Leonhardt <apleonhardt@gmail.com>
2011-09-20 13:09:26 +0200

    Perform minor refactoring for NEO classes

--- src/main/scala/neo.scala
+++ src/main/scala/neo.scala
@@ -1,7 +1,12 @@
 package org.gnode.lib.neo
 
-case class NEOData(units: String, values: List[Double]) {
+case class NEOData(units: String,
+		   values: List[Double]) {
+
   def this(units: String, data: Double) = this(units, List(data))
+
 }
 
-class NEObject(val stringInfo: Map[String, String], val numInfo: Map[String, Double], val data: Map[String, NEOData])
+class NEObject(val stringInfo: Map[String, String],
+	       val numInfo: Map[String, Double],
+	       val data: Map[String, NEOData]) {
