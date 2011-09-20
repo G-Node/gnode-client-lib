c58cb3a5de6e4da429ca18965b3b251cef6e8911
Aljoscha Leonhardt <apleonhardt@gmail.com>
2011-09-20 15:29:13 +0200

    Implement NEOBuilder

--- src/main/scala/neo.scala
+++ src/main/scala/neo.scala
@@ -7,6 +7,26 @@ case class NEOData(units: String,
 
 }
 
-class NEObject(val stringInfo: Map[String, String],
-	       val numInfo: Map[String, Double],
-	       val data: Map[String, NEOData])
+class NEObject(val stringInfo: Map[String, String] = Map[String, String](),
+	       val numInfo: Map[String, Double] = Map[String, Double](),
+	       val data: Map[String, NEOData] = Map[String, NEOData](),
+	       val relations: Map[String, List[String]] = Map[String, List[String]]())
+
+class NEOBuilder {
+
+  import collection.mutable.{Map => MuMap}
+
+  private val strings = MuMap[String, String]()
+  private val numbers = MuMap[String, Double]()
+  private val data = MuMap[String, NEOData]()
+  private val rel = MuMap[String, List[String]]()
+
+  def add(k: String, v: String) { strings += k -> v }
+  def add(k: String, v: Double) { numbers += k -> v }
+  def add(k: String, v: NEOData) { data += k -> v }
+  def add(k: String, v: List[String]) { rel += k -> v }
+
+  def build: NEObject =
+    new NEObject(Map.empty ++ strings, Map.empty ++ numbers, Map.empty ++ data, Map.empty ++ rel)
+
+}
