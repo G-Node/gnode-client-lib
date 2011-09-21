1653af7be206f18c6491751cde070d5a2cb2cd85
Aljoscha Leonhardt <apleonhardt@gmail.com>
2011-09-21 16:08:57 +0200

    Write push() method for upload class

--- src/main/scala/client/upload.scala
+++ src/main/scala/client/upload.scala
@@ -1,12 +1,12 @@
 package org.gnode.lib.client
 
 // Internal packages
-import org.gnode.lib.cache._
 import org.gnode.lib.util._
 import org.gnode.lib.conf._
 import org.gnode.lib.neo._
 import org.gnode.lib.api._
 import org.gnode.lib.parse._
+import org.gnode.lib.validate._
 
 // External packages
 import dispatch._
@@ -14,4 +14,45 @@ import dispatch._
 // Make log messages available globally
 import LogMessages._
 
-class Uploader(private val config: Configuration, private val http: Http) extends BatchTransfer
+class Uploader(private val config: Configuration, private val http: Http) extends BatchTransfer {
+
+  // private def pushNew(obj: NEObject): Option[String] = {
+   
+  // }
+    
+  // private def pushExisting(id: String, obj: NEObject): Boolean
+
+  // def add(id: String, obj: NEObject) {
+  //   jobs enqueue Job(id, Some(obj))
+  //   logger info JOB_ADD(id)
+  // }
+
+  // def push() = {
+
+  //   import scala.collection.mutable.ListBuffer
+  //   val b = ListBuffer[String]()
+
+  //   while (!jobs.isEmpty) {
+
+  //     val job = jobs.dequeue
+
+  //     job.id match {
+  // 	case "" =>
+  // 	  pushNew(job.obj.get) match {
+  // 	    case Some(id) => logger info JOB_COMPLETE(id); b += id
+  // 	    case None => logger error JOB_FAILURE("NEW_OBJECT")
+  // 	  }
+  // 	case id: String =>
+  // 	  pushExisting(id, job.obj.get) match {
+  // 	    case true => logger info JOB_COMPLETE(id)
+  // 	    case false => logger error JOB_FAILURE(id)
+  // 	  }
+  //     }
+
+  //   }
+
+  //   Some(b.toList)
+
+  // }
+
+}
