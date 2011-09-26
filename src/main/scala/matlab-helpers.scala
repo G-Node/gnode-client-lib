ba8c5c116ac023e476d2acf302ff986c975463aa
Aljoscha Leonhardt <apleonhardt@gmail.com>
2011-09-26 10:14:10 +0200

    Implement guessType() function on MATLAB helper object (based on APICaller)

--- src/main/scala/matlab-helpers.scala
+++ src/main/scala/matlab-helpers.scala
@@ -1,5 +1,7 @@
 package org.gnode.lib.matlab
 
+import org.gnode.lib.api._
+
 object Helper {
 
   def isNone[T](that: Option[T]) =
@@ -7,5 +9,13 @@ object Helper {
       case None => true
       case _ => false
     }
+
+  def guessType(id: String) = {
+    val splitter = new APIHelper {}
+    val parts = splitter.split(id)
+
+    if (parts._1.isEmpty) throw new Exception("Could not approximate object type")
+    else parts._1
+  }
 
 }
